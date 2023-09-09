c8e68842d08a4af9427724f1eb94bb894b479c60
Facu ICC <facui.conci97@gmail.com>
2023-09-09 18:32:56 -0300

    ajustando ambientes de la aplicacion

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
+const { argv } = require("node:process");
+
+process.env.NODE_ENV = argv[2];
+
 const app = require("./app");
 const realTimeServer = require("./socket");
 
 function main() {
+  console.log(`env: ${process.env.NODE_ENV}`);
+  
   const httpServer = app.listen(app.get("port"), (error) => {
     if (error) {
       console.error(error);
--- src/utils/app-config.js
+++ src/utils/app-config.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
+const { join } = require("path");
+require("dotenv").config({
+  path: join(process.cwd(), `.env.${process.env.NODE_ENV}`),
+});
 
 const appConfig = {
-    host: process.env.APP_HOST || 'localhost',
-    port: process.env.APP_PORT || 8080,
-    sessionSecret: process.env.SESSION_SECRET,
-    mongodbAtlas: {
-        dbname: process.env.MONGO_ATLAS_DATABASE,
-        user: process.env.MONGO_ATLAS_USER,
-        pssw: process.env.MONGO_ATLAS_PASSWORD,
-    }
-}
+  host: process.env.APP_HOST || "localhost",
+  port: process.env.APP_PORT || 8080,
+  sessionSecret: process.env.SESSION_SECRET,
+  mongodbAtlas: {
+    dbname: process.env.MONGO_ATLAS_DATABASE,
+    user: process.env.MONGO_ATLAS_USER,
+    pssw: process.env.MONGO_ATLAS_PASSWORD,
+  },
+};
 
 module.exports = appConfig;
